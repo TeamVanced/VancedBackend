f132a0cd0ff2c15e877f2416f6e0c9fe797d9c3e
Mattis6666 <sgtmattis@gmail.com>
2020-04-25 00:29:17 +0200

    fix paths

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-
 // app.get('/api/v1/site/edit', async (req, res) => {
 //     const articles = await Article.find().sort({ createdAt: 'desc' })
 //     res.render('api/index', { articles: articles })
@@ -9,8 +8,8 @@ const app = express();
 app.use(express.json());
 
 // app.use('/', require('./website/index'))
-app.use('/api/v1/versions/', require('./api/vancedversions'));
-app.use('/api/v1/changelog/', require('./api/vancedchangelog'));
+app.use('/api/v1/versions/', require('./api/versions').router);
+app.use('/api/v1/changelog/', require('./api/changeLog').router);
 
 const port = 4000;
 app.listen(port, () => console.log('Server Started on port' + port));
